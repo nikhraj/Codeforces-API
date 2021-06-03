a258dd1c4a5b3b292a111e9495bdbb539b83c0da
Nikhil Rajput <55176863+nikhraj@users.noreply.github.com>
2021-06-03 12:11:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -33,18 +33,18 @@ class problem_response(BaseModel):
     samples: list = []
     note: str
 
-@app.get("/")
+@app.get("/",methods=['GET'])
 async def root():
     return {"title":"Codeforces API", "version":"1.0","Made by":"Prakhar Jain"}
 
 
-@app.get("/contests/{contest_id}", response_model=contest_response)
+@app.get("/contests/{contest_id}",methods=['GET'], response_model=contest_response)
 async def contest_data(contest_id: int):
 
     results =  codeforces_contest_wrapper.parse_contest(CONTEST_LINK + str(contest_id))
     return results
 
-@app.get("/problems/{contest_id}/{problem_id}", response_model=problem_response)
+@app.get("/problems/{contest_id}/{problem_id}",methods=['GET'], response_model=problem_response)
 async def problem_data(contest_id: int,problem_id: str):
     
     results = codeforces_problem_wrapper.parse_problem(PROBLEM_LINK + str(contest_id) + '/' + problem_id)
