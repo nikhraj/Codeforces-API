87b84ebab4bcdc114ef6c43c318a667c9707477c
Nikhil Rajput <55176863+nikhraj@users.noreply.github.com>
2021-06-04 17:49:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import codeforces_problem_wrapper
 import codeforces_contest_wrapper
-
+import flask
 
 PROBLEM_LINK = 'https://codeforces.com/problemset/problem/'
 CONTEST_LINK = 'https://codeforces.com/contest/'
@@ -12,3 +12,6 @@ app = flask.Flask(__name__)
 def contest_data():
     contest_id = flask.request.args['id']
     return flask.jsonify( codeforces_contest_wrapper.parse_contest(CONTEST_LINK + contest_id))
+
+if __name__ == '__main__':
+    app.run()
