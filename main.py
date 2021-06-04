78f440042f50c735fa2cd2502639bfba6ab77f3f
Nikhil Rajput <55176863+nikhraj@users.noreply.github.com>
2021-06-04 17:33:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,51 +1,14 @@
-from fastapi import FastAPI
 import codeforces_problem_wrapper
 import codeforces_contest_wrapper
-from pydantic import BaseModel
-from typing import List
 
 
 PROBLEM_LINK = 'https://codeforces.com/problemset/problem/'
 CONTEST_LINK = 'https://codeforces.com/contest/'
 
 
-app = FastAPI(
-    title="Codeforces API",
-    version="1.0",
-)
-
-
-class contest_response(BaseModel):
-    contest_id: int
-    contest_name: str
-    contest_link: str
-    problems: list = []
-    editorial: str
-    announcement: str
-
-class problem_response(BaseModel):
-    title: str
-    timeLimit: dict = {}
-    memoryLimit: dict = {}
-    statement: str
-    inputSpecification: str
-    outputSpecification: str
-    samples: list = []
-    note: str
+app = flask.Flask(__name__)
 
 @app.route("/",methods=['GET'])
-async def root():
-    return {"title":"Codeforces API", "version":"1.0","Made by":"Prakhar Jain"}
-
-
-@app.route("/contests/{contest_id}",methods=['GET'], response_model=contest_response)
-async def contest_data(contest_id: int):
-
-    results =  codeforces_contest_wrapper.parse_contest(CONTEST_LINK + str(contest_id))
-    return results
-
-@app.route("/problems/{contest_id}/{problem_id}",methods=['GET'], response_model=problem_response)
-async def problem_data(contest_id: int,problem_id: str):
-    
-    results = codeforces_problem_wrapper.parse_problem(PROBLEM_LINK + str(contest_id) + '/' + problem_id)
-    return results
+def contest_data():
+    contest_id = flask.request.args['id']
+    return flask.jsonify( codeforces_contest_wrapper.parse_contest(PROBLEM_LINK + contest_id))
